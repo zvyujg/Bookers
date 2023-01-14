7dbaf99502a9957b11fe8678965da2c7ebd457da
EC2 Default User <120617980+zvyujg@users.noreply.github.com>
2023-01-14 16:00:35 +0000

    Second Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,9 +10,11 @@ class BooksController < ApplicationController
   end
   
   def index
+    @books =Book.all
   end
 
   def show
+    @book =Book.find(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'books/new'
   post "books" => 'books#create'
-  get 'books/index'
+  get 'books' => 'books#index'
   get 'books/show'
   get 'books/edit'
   get '/top' => 'homes#top'
+  get 'books/:id'=>'books#show',as:'book'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
