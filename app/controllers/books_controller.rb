06ccb63866b56fb69e087e1efdfe16dab0b66527
EC2 Default User <120617980+zvyujg@users.noreply.github.com>
2023-01-17 17:00:49 +0000

    Four Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,12 @@ class BooksController < ApplicationController
     @book =Book.find(params[:id])
   end
   
+  def destroy
+    book =Book.find(params[:id])
+    book .destroy
+    redirect_to '/books'
+  end
+  
   def update
     book=Book.find(params[:id])
     book.update(book_params)
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   get 'books/new'
   get '/top' => 'homes#top'
+  resources :books
   post "books" => 'books#create'
   get 'books' => 'books#index'
   get 'books/:id'=>'books#show',as:'book'
-  get 'books/edit'=>'books#edit',as:'edit_book'
+  get 'books/:id/edit'=>'books#edit',as:'edit_book'
   patch'books/:id' =>'books#update',as:'update_book'
+  delete 'books/:id'=> 'books#destroy' ,as:'destroy_book'
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
